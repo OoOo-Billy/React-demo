d7e056b54ab3a5de9a76cfecd6c9f5e088383ba7
Billy <leiyu_bi@foxmail.com>
2019-09-02 17:44:46 +0800

    优化随机函数

--- src/utils/tools.js
+++ src/utils/tools.js
@@ -4,7 +4,7 @@ export default{
     const chars = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
     let res = ''
     for (let i = 0; i < num; i++) {
-      var id = Math.ceil(Math.random() * 9)
+      var id = Math.round(Math.random() * 9)
       res += chars[id]
     }
     return res
